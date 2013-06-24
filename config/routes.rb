27b00429b6478f68decddc76a9dc5859c80607ff
zaziemo <zaziemo@gmail.com>
2013-06-24 15:24:51 +0200

    improvements and debugging layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -50,4 +54,3 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
